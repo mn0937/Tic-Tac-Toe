40624873637317ea4fe948062644788dd4b328cb
mn0937 <mn0937@srmist.edu.in>
2026-04-21 09:26:39 +0530

    Update Tic-Tac-Toe.java

--- Tic-Tac-Toe.java
+++ Tic-Tac-Toe.java
@@ -5,16 +5,16 @@ int main() {
     int i, j;
 
     // Initialize the board with '-'
-    for(i = 0; i < 3; i++) {
-        for(j = 0; j < 3; j++) {
+    for (i = 0; i < 3; i++) {
+        for (j = 0; j < 3; j++) {
             board[i][j] = '-';
         }
     }
 
     // Print the board
     printf("Tic-Tac-Toe Board:\n\n");
-    for(i = 0; i < 3; i++) {
-        for(j = 0; j < 3; j++) {
+    for (i = 0; i < 3; i++) {
+        for (j = 0; j < 3; j++) {
             printf("%c ", board[i][j]);
         }
         printf("\n");
